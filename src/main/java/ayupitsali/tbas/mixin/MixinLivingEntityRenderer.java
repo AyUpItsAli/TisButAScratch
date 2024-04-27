b813c677b307ac9365c7dcf1487ceda7650cfc2c
Ali <ali.dee1410@gmail.com>
2024-04-28 00:36:07 +0100

    Add Mixin

---
+++ src/main/java/ayupitsali/tbas/mixin/MixinLivingEntityRenderer.java
@@ -0,0 +1,19 @@
+package ayupitsali.tbas.mixin;
+
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(LivingEntityRenderer.class)
+public class MixinLivingEntityRenderer {
+    /**
+     * @author AyUpItsAli
+     * @reason Always return as if the entity is not hurt (disabling the red flash when taking damage)
+     */
+    @Overwrite
+    public static int getOverlayCoords(LivingEntity pLivingEntity, float pU) {
+        return OverlayTexture.pack(pU, false);
+    }
+}
